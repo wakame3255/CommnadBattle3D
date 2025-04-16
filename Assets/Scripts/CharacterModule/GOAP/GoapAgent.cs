ef57c8f68d841490752317088e52b0e0a57e0545
wakame3255 <46024199+wakame3255@users.noreply.github.com>
2025-04-16 18:17:25 +0900

    Goapにおけるプランナーの構築

--- Assets/Scripts/CharacterModule/GOAP/GoapAgent.cs
+++ Assets/Scripts/CharacterModule/GOAP/GoapAgent.cs
@@ -1,52 +1,21 @@
 using System;
 using UnityEngine;
 using System.Collections.Generic;
-public class GoapAgent : MonoBehaviour 
+public class GoapAgent
 {
+    private AllCharacterStatus _allCharacterStatus;
 
-    //現在の状態
-    private Dictionary<WorldStateKey, object> _worldState = new Dictionary<WorldStateKey, object>();
+    private CountdownTimer _statusTimer;
 
-    //目標状態
-    private Dictionary<WorldStateKey, object> _goal = new Dictionary<WorldStateKey, object>();
+    private AgentGoal _lastGoal;
 
-    //アクションのリスト
-    private List<GoapActionBase> _actions = new List<GoapActionBase>();
+    private AgentGoal _currentGoal;
 
-    //現在のアクション
-    private Queue<GoapActionBase> _currentPlan;
 
-    private void Awake()
+    public GoapAgent(AllCharacterStatus allCharacterStatus)
     {
-        //アクションの生成
-        _actions.Add(gameObject.AddComponent<AttackForEnemyAction>());
-        _actions.Add(gameObject.AddComponent<ApproachForEnemyAction>());
-
-        foreach(GoapActionBase goapAction in _actions)
-        {
-            goapAction.Setup();
-        }
-
-        //初期状態
-        _worldState.Add(WorldStateKey.EnemyIsInRange, false);
-
-        //ゴールの設定
-        _goal = new Dictionary<WorldStateKey, object>();
-        _goal.Add(WorldStateKey.EnemyIsInRange, true);
+       _allCharacterStatus = allCharacterStatus;
     }
 
-    private void Start()
-    {
-        
-    }
-
-    private void Update()
-    {
-        if (_currentPlan == null || _currentPlan.Count == 0)
-        {
-
-        }
-    }
-
-
+    
 }
--- Assets/Scripts/CharacterModule/GOAP/GoapPlanner.cs
+++ Assets/Scripts/CharacterModule/GOAP/GoapPlanner.cs
@@ -1,10 +1,77 @@
+using System.Collections.Generic;
+using System.Linq;
+
 public interface IGoapPlanner
 {
- 
+    public ActionPlan Plan(GoapAgent agent, HashSet<AgentGoal> goals, AgentGoal mostRecentGoal = null);
 }
-public class GoapPlanner
+    public class GoapPlanner : IGoapPlanner
 {
+    public ActionPlan Plan(GoapAgent agent, HashSet<AgentGoal> goals, AgentGoal mostRecentGoal = null)
+    {
+        List<AgentGoal> agentGoals = goals
+            .Where(g => g.DesiredEffects.Any(b => !b.Evaluate()))
+            .OrderByDescending(g => g == mostRecentGoal ? g.Priority - 0.01 : g.Priority)
+            .ToList();
 
+        foreach(AgentGoal goal in agentGoals)
+        {
+            Goap.Node goalNode = new Goap.Node(null, null, goal.DesiredEffects, 0);
+        }
+    }
+
+    private bool FindPath(Goap.Node parent, HashSet<AgentAction> actions)
+    {
+        //コスト順にアクションを並べ替え
+        IOrderedEnumerable<AgentAction> orderedActions = actions.OrderBy(a => a.Cost);
+
+        foreach(AgentAction action in orderedActions)
+        {
+            HashSet<AgentBelief> requiredEffects = parent.RequiredEffects;
+
+            //すでに達成している信念をリストから排除
+            requiredEffects.RemoveWhere(b => b.Evaluate());
+
+            //信念が積まれていない場合は終了
+            if (requiredEffects.Count == 0)
+            {
+                return true;
+            }
+
+            //特定の信念を達成できるアクションを割り出す。
+            if (action.Effects.Any(requiredEffects.Contains))
+            {
+                HashSet<AgentBelief> newRequiredEffects = new HashSet<AgentBelief>(requiredEffects);
+                //達成するであろう信念を排除
+                newRequiredEffects.ExceptWith(action.Effects);
+                //アクションを実行するために必要な信念を追加
+                newRequiredEffects.UnionWith(action.Preconditions);
+
+                //目標達成につながらないアクションはスキップ（消すのではなくboolを返す関数があってもいいのでは）
+                HashSet<AgentAction> newAvailableActions = new HashSet<AgentAction>(actions);
+                newAvailableActions.Remove(action);
+
+                //新たな状態になるために必要なノードの生成
+                Goap.Node newNode = new Goap.Node(parent, action, newRequiredEffects, parent.Cost + action.Cost);
+
+                //再帰的に必要なアクションを探査する
+                if (FindPath(newNode, newAvailableActions))
+                {
+                    parent.Leaves.Add(newNode);
+                    newRequiredEffects.ExceptWith(newNode.Action.Preconditions);
+                }
+
+                //新たな状態を達成するために必要な信念が残っている場合は、次のアクションを探す
+                if (newRequiredEffects.Count == 0)
+                {
+                    return true;
+                }
+            }
+        }
+
+        //目標を達成するために必要な経路がある場合はtrueを返す
+        return parent.Leaves.Count > 0;
+    }
 }
 
 namespace Goap
@@ -12,6 +79,33 @@ namespace Goap
     public class Node
     {
         public Node Parent { get; }
-    }
+        public AgentAction Action { get; }
+        public HashSet<AgentBelief> RequiredEffects { get; }
+        public float Cost { get; }  
+        public List<Node> Leaves { get; } 
 
+        public Node(Node parent, AgentAction action, HashSet<AgentBelief> effects, float cost)
+        {
+            Action = action;
+            Parent = parent;
+            RequiredEffects = new HashSet<AgentBelief>(effects);
+            Leaves = new List<Node>();
+            Cost = cost;
+        }
+    }
 }
+
+public class ActionPlan
+{
+    public AgentGoal Goal { get; }
+    public Stack<AgentAction> Actions { get; }
+    public Node TotalNode { get; }
+
+    public ActionPlan(AgentGoal goal, Stack<AgentAction> actions, Node totalNode)
+    {
+        Goal = goal;
+        Actions = actions;
+        TotalNode = totalNode;
+    }
+}
+
